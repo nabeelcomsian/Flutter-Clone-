b7906c557348ba51d00fe5a27063971208204317
NABEEL SALEEM <nabeelsaleem893@gmail.com>
2025-02-12 17:54:36 +0500

    remove all the errors complted the signup screen with complete front and back end

--- lib/Screens/signup_screen.dart
+++ lib/Screens/signup_screen.dart
@@ -1,6 +1,11 @@
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:instagram/Utils/colors.dart';
+import 'package:instagram/Utils/image_picker.dart';
 import 'package:instagram/resources/auth_method.dart';
 import 'package:instagram/widgets/text_field_input.dart';
 import 'package:instagram/resources/auth_method.dart';
@@ -17,7 +22,8 @@ class _SignupScreenState extends State<SignupScreen> {
   final TextEditingController _passwordcontroller = TextEditingController();
   final TextEditingController _biocontroller = TextEditingController();
   final TextEditingController _userNamecontroller = TextEditingController();
-
+  Uint8List? image;
+  bool isLoading = false;
   @override
   void dispose() {
     _emailcontroller.dispose();
@@ -26,6 +32,37 @@ class _SignupScreenState extends State<SignupScreen> {
     _userNamecontroller.dispose();
 
     super.dispose();
+  }
+
+  selectedImage() async {
+    Uint8List img = await pickImage(ImageSource.gallery);
+    if (img != null) {
+      setState(() {
+        image = img;
+        debugPrint("Image selected: ${image!.length} bytes");
+      });
+    } else {
+      debugPrint("No image selected");
+    }
+  }
+
+  void signupUser() async {
+    setState(() {
+      isLoading = true;
+    });
+    final response = await AuthMethod().signUpUser(
+        userName: _userNamecontroller.text,
+        email: _emailcontroller.text,
+        bio: _biocontroller.text,
+        password: _passwordcontroller.text);
+    debugPrint('here is the reponse when signup hit $response');
+
+    if (response != 'success' && mounted) {
+      showSnackBar(response, context);
+    }
+    setState(() {
+      isLoading = false;
+    });
   }
 
   @override
@@ -50,16 +87,21 @@ class _SignupScreenState extends State<SignupScreen> {
             ),
             Stack(
               children: [
-                const CircleAvatar(
+                CircleAvatar(
                   radius: 64,
-                  backgroundImage: NetworkImage(
-                      'https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&q=70&fm=webp'),
+                  backgroundImage: image != null
+                      ? MemoryImage(image!) // Use the selected image
+                      : const NetworkImage(
+                          // Fallback to a default image
+                          'https://t3.ftcdn.net/jpg/00/64/67/80/360_F_64678017_zUpiZFjj04cnLri7oADnyMH0XBYyQghG.webp',
+                        ) as ImageProvider,
                 ),
                 Positioned(
                     top: 85,
                     left: 90,
                     child: IconButton(
-                        onPressed: () {}, icon: const Icon(Icons.add_a_photo)))
+                        onPressed: selectedImage,
+                        icon: const Icon(Icons.add_a_photo)))
               ],
             ),
             const SizedBox(
@@ -99,14 +141,7 @@ class _SignupScreenState extends State<SignupScreen> {
               height: 24,
             ),
             InkWell(
-              onTap: () async {
-                final response = await AuthMethod().signUpUser(
-                    userName: _userNamecontroller.text,
-                    email: _emailcontroller.text,
-                    bio: _biocontroller.text,
-                    password: _passwordcontroller.text);
-                debugPrint('here is the reponse when signup hit $response');
-              },
+              onTap: signupUser,
               child: Ink(
                 // alignment: Alignment.center,
                 padding: const EdgeInsets.symmetric(vertical: 12),
@@ -115,13 +150,19 @@ class _SignupScreenState extends State<SignupScreen> {
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(5))),
                 width: double.infinity,
-                child: const Center(
-                  child: Text(
-                    'Sign Up',
-                    style: TextStyle(
-                        color: Colors.white), // Optional: Set text color
-                  ),
-                ),
+                child: isLoading
+                    ? const Center(
+                        child: CircularProgressIndicator(
+                          color: primaryColor,
+                        ),
+                      )
+                    : const Center(
+                        child: Text(
+                          'Sign Up',
+                          style: TextStyle(
+                              color: Colors.white), // Optional: Set text color
+                        ),
+                      ),
               ),
             ),
             const SizedBox(
---
+++ lib/Utils/image_picker.dart
@@ -0,0 +1,21 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+pickImage(ImageSource source) async {
+  final ImagePicker imagerPicker = ImagePicker();
+  XFile? _file = await imagerPicker.pickImage(source: source);
+  if (_file != null) {
+    return await _file.readAsBytes();
+  }
+  return debugPrint("No file selected from gallery");
+}
+
+showSnackBar(String content, BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(content),
+    ),
+  );
+}
